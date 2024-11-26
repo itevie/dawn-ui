a297ad35df536c59b56e057b6dcaa81923b46f7f
Isabella <izzyplays07@gmail.com>
2024-11-26 13:54:12 +0000

    asd

--- util.ts
+++ util.ts
@@ -178,3 +178,19 @@ export type ArrayElement<ArrayType extends readonly unknown[]> =
 export function leftPad(what: string, w: string, length: number) {
   return w.repeat(length - what.length || 0) + what;
 }
+
+export function randomBoolean(): boolean {
+  return Math.random() > 0.5;
+}
+
+export function hexToRGB(hex: string, alpha: number) {
+  var r = parseInt(hex.slice(1, 3), 16),
+    g = parseInt(hex.slice(3, 5), 16),
+    b = parseInt(hex.slice(5, 7), 16);
+
+  return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+}
+
+export function randomRange(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
