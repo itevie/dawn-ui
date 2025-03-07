51e18ea51cfbdcde9175cdf08296c9a1bf862eba
Isabella <izzyplays07@gmail.com>
2025-03-07 19:25:44 +0000

    refactor: update FAB component to accept ReactNode for label and add FullWidthInput component style: adjust secondary background color and add dawn-container-secondary styles

--- components/FAB.tsx
+++ components/FAB.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { ReactNode, useEffect, useRef } from "react";
 import { combineClasses } from "../util";
 import Button, { ButtonType } from "./Button";
 import { setShortcutCallback } from "./ShortcutManager";
@@ -10,7 +10,7 @@ export default function FAB({
   shortcut,
 }: {
   clicked?: () => void;
-  label?: string;
+  label?: ReactNode;
   type?: ButtonType;
   shortcut?: string;
 }) {
